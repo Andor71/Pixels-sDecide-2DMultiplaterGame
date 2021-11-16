982a95342d3441969f4eb5669faa88c6ec896497
Andor71 <werozg71@gmail.com>
2021-11-16 23:12:04 +0200

    BackGround and Arena Design

--- Pixel's Decide/Assets/Scripts/Character_Health.cs
+++ Pixel's Decide/Assets/Scripts/Character_Health.cs
@@ -6,17 +6,25 @@ using Photon.Pun;
 public class Character_Health : MonoBehaviourPunCallbacks, IPunObservable
 {
     
+    PhotonView view;
     public int maxHealth = 0;
+    public int globalDamageDamage = 1;
+    public float timeGlobalDamage = 5;
+    float timeRGlobalDamage = 0;
     float currentHealth;
+    public bool stayingInArena = false;
     Transform healthBar;
     SpriteRenderer spriteRendererHP;
-  
+    Collision_Detector collision_Detector;
+
     void Start()
     {
         healthBar = transform.Find("HealthBar");
         currentHealth = maxHealth;
         spriteRendererHP = healthBar.GetComponent<SpriteRenderer>();
         spriteRendererHP.enabled = false;
+        collision_Detector = GetComponent<Collision_Detector>();
+        view = GetComponent<PhotonView>();
         healthBar.localScale = new Vector3(currentHealth,2,1);
     }
 
@@ -24,13 +32,45 @@ public class Character_Health : MonoBehaviourPunCallbacks, IPunObservable
     {
         if(currentHealth < maxHealth){
             spriteRendererHP.enabled = true;
+            spriteRendererHP.color = Color.green;
         }
-        healthBar.localScale = new Vector3(currentHealth,1,1);
+        healthBar.localScale = new Vector3(currentHealth/10,1,1);
+        
+        if(currentHealth < maxHealth*50/100){
+            spriteRendererHP.color = Color.yellow;
+        }
+
+        if(currentHealth < maxHealth*25/100){
+            spriteRendererHP.color = Color.red;
+        }
+        if(currentHealth == maxHealth){
+            spriteRendererHP.enabled = false;
+        }
+
+
+        if(view.IsMine){
+            if(collision_Detector.getGlobalDamage() && !stayingInArena)
+            {
+                if(Time.time > timeRGlobalDamage){
+                    timeRGlobalDamage = Time.time + timeGlobalDamage;
+                    view.RPC("gotHit",RpcTarget.All,globalDamageDamage);
+                }
+            }
+        }
     }
+
     [PunRPC]
     public void gotHit(int valueOfDamage)
     {
+        Debug.Log("Got Hit");
         currentHealth -= valueOfDamage;
+    }
+
+    [PunRPC]
+    public void Heal()
+    {
+        Debug.Log("Healed");
+        currentHealth = maxHealth;
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
